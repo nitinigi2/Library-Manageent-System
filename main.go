ac7e80a44a7acc128aea831457c1c335d09f2e37
Nitin Singhal <nitinsinghal139@gmail.com>
2021-07-23 16:37:23 +0530

    Sample hello-world

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Main function called")
+	handleRequests()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
